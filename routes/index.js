1944e34962181a3d04f5412ccc0235672baabdf1
Ivo Nikić <nikiciv@gmail.com>
2020-06-21 17:04:15 +0300

    Dashboard stories

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
+const Story = require("../models/Story");
+
 // Login/Landing page
 // @route GET /ensureGuest
 router.get("/", ensureGuest, (req, res) => {
@@ -12,10 +14,17 @@ router.get("/", ensureGuest, (req, res) => {
 
 // Dashboard page
 // @route GET /dashboard
-router.get("/dashboard", ensureAuth, (req, res) => {
-  res.render("dashboard", {
-    name: req.user.firstName,
-  });
+router.get("/dashboard", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean();
+    res.render("dashboard", {
+      name: req.user.firstName,
+      stories,
+    });
+  } catch (error) {
+    console.error(error);
+    res.render("error/500");
+  }
 });
 
 module.exports = router;
