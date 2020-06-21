95705e83b0c45da41051797518bd1c3272cf5fc4
Ivo Nikić <nikiciv@gmail.com>
2020-06-21 20:18:48 +0300

    Add method override for put req

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
 const passport = require("passport");
+const methodOverride = require("method-override");
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 
@@ -25,6 +26,18 @@ const app = express();
 // Body parser
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
 
 // Logging - use only in development mode
 if (process.env.NODE_ENV === "development") {
